6fb49737f94d23b761bbd0f97a5d48ace5030d94
bianca-rodrigues <biancapereirarodrigues02@gmail.com>
2022-10-13 14:08:19 -0300

    tabuada

---
+++ exer1064.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ 
+int main() 
+{
+    float media, num1, num2, num3, num4, num5, num6;
+    int posit=0;
+
+    scanf("%f %f %f %f %f %f", &num1, &num2, &num3, &num4, &num5, &num6);
+
+    if (num1>0)
+    {
+        
+    }
+    
+    
+
+    
+
+
+    printf("%i valores positivos\n", posit);
+    printf("%.1f\n", media);
+
+ 
+    return 0;
+}
