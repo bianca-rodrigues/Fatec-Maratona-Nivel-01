27b2f726a306a86de9e58359e9220e39b5f5416b
bianca-rodrigues <biancapereirarodrigues02@gmail.com>
2022-10-13 13:30:13 -0300

    numeros pares

---
+++ exer1059.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() 
+{
+    int i, num=0;
+
+    for (i = 1; i <= 50; i++)
+    {
+        num = num+2;
+
+        printf("%i\n", num);
+    }
+    
+ 
+    return 0;
+}
