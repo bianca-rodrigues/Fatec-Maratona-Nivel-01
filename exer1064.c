ead234d544a8284dc6d56bfe9b426cd7eb81f807
bianca-rodrigues <biancapereirarodrigues02@gmail.com>
2022-10-17 17:28:33 -0300

    positivos e media

--- exer1064.c
+++ exer1064.c
@@ -2,20 +2,43 @@
  
 int main() 
 {
-    float media, num1, num2, num3, num4, num5, num6;
+    float media, num1, num2, num3, num4, num5, num6, soma=0;
     int posit=0;
 
     scanf("%f %f %f %f %f %f", &num1, &num2, &num3, &num4, &num5, &num6);
 
     if (num1>0)
     {
-        
+        posit = posit + 1;
+        soma = soma + num1;
     }
-    
-    
+    if (num2>0)
+    {
+        posit = posit + 1;
+        soma = soma + num2;
+    }
+    if (num3>0)
+    {
+        posit = posit + 1;
+        soma = soma + num3;
+    }
+    if (num4>0)
+    {
+        posit = posit + 1;
+        soma = soma + num4;
+    }
+    if (num5>0)
+    {
+        posit = posit + 1;
+        soma = soma + num5;
+    }
+    if (num6>0)
+    {
+        posit = posit + 1;
+        soma = soma + num6;
+    }
 
-    
-
+    media = soma/posit;
 
     printf("%i valores positivos\n", posit);
     printf("%.1f\n", media);
