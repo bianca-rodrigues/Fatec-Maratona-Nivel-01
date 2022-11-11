21c4e6dd6613a28d9395166abc2a2173196083b9
bianca-rodrigues <biancapereirarodrigues02@gmail.com>
2022-11-10 21:55:49 -0300

    notas e moedas

--- exer1021.c
+++ exer1021.c
@@ -3,89 +3,86 @@
 int main()
 {
 
-double n;
-int cem, cinquenta, vinte, dez, cinco, dois,um, cinqCents, vinCincoCents, dezCents, cincoCents, umCents, nota, resto;
+    double n;
+    int cem, cinquenta, vinte, dez, cinco, dois,um, cinqCents, vinCincoCents, dezCents, cincoCents, umCents, nota, resto;
 
-scanf("%lf", &n);
+    scanf("%lf", &n);
 
-nota = n;
+    nota = n*100;
 
-cem =  nota/ 100;
+    cem =  nota/ 10000;
 
-resto = nota % 100;
+    resto = nota % 10000;
 
-cinquenta = resto/50;
+    cinquenta = resto/5000;
 
-resto = resto%50;
+    resto = resto%5000;
 
-vinte = resto/20;
+    vinte = resto/2000;
 
-resto = resto%20;
+    resto = resto%2000;
 
-dez = resto/10;
+    dez = resto/1000;
 
-resto = resto%10;
+    resto = resto%1000;
 
-cinco = resto/5;
+    cinco = resto/500;
 
-resto = resto%5;
+    resto = resto%500;
 
-dois = resto/2;
+    dois = resto/200;
 
-resto = resto%2;
+    resto = resto%200;
 
-um = resto/1;
+    um = resto/100;
 
-resto = resto%1;
+    resto = resto%100;
 
-resto = resto*100;
+    cinqCents = resto/50;
 
-cinqCents = resto/50;
+    resto = resto%50;
 
-resto = resto%50;
+    vinCincoCents = resto/25;
 
-vinCincoCents = resto/25;
+    resto = resto%25;
 
-resto = resto%25;
+    dezCents = resto/10;
 
-dezCents = resto/10;
+    resto = resto%10;
 
-resto = resto%10;
+    cincoCents = resto/5;
 
-cincoCents = resto/5;
+    resto = resto%5;
 
-resto = resto%5;
+    umCents = resto/1;
 
-umCents = resto/5;
+    printf ("NOTAS:\n");
 
-printf ("NOTAS:\n");
+    printf ("%i nota(s) de R$ 100.00\n", cem);
 
-printf ("%i nota(s) de R$ 100.00\n", cem);
+    printf ("%i nota(s) de R$ 50.00\n", cinquenta);
 
-printf ("%i nota(s) de R$ 50.00\n", cinquenta);
+    printf ("%i nota(s) de R$ 20.00\n", vinte);
 
-printf ("%i nota(s) de R$ 20.00\n", vinte);
+    printf ("%i nota(s) de R$ 10.00\n", dez);
 
-printf ("%i nota(s) de R$ 10.00\n", dez);
+    printf ("%i nota(s) de R$ 5.00\n", cinco);
 
-printf ("%i nota(s) de R$ 5.00\n", cinco);
+    printf ("%i nota(s) de R$ 2.00\n", dois);
 
-printf ("%i nota(s) de R$ 2.00\n", dois);
+    printf ("MOEDAS:\n");
 
-printf ("MOEDAS:\n");
+    printf ("%i moeda(s) de R$ 1.00\n", um);
 
-printf ("%i moeda(s) de R$ 1.00\n", um);
+    printf ("%i moeda(s) de R$ 0.50\n", cinqCents);
 
-printf ("%i moeda(s) de R$ 0.50\n", cinqCents);
+    printf ("%i moeda(s) de R$ 0.25\n", vinCincoCents);
 
-printf ("%i moeda(s) de R$ 0.25\n", vinCincoCents);
+    printf ("%i moeda(s) de R$ 0.10\n", dezCents);
 
-printf ("%i moeda(s) de R$ 0.10\n", dezCents);
+    printf ("%i moeda(s) de R$ 0.05\n", cincoCents);
 
-printf ("%i moeda(s) de R$ 0.05\n", cincoCents);
-
-printf ("%i moeda(s) de R$ 0.01\n", umCents);
-
+    printf ("%i moeda(s) de R$ 0.01\n", umCents);
 
     return 0;
 }
